22a275c0b3ae887e2f6bf208c57733ceece7ee2d
Dorian Wilfred Lanuza Smith <dorian.lanuza2022@gmail.com>
2026-03-05 16:18:56 -0600

    test

--- NextOps.Api/Dtos/CreateProductDto.cs
+++ NextOps.Api/Dtos/CreateProductDto.cs
@@ -1,11 +1,13 @@
 using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.Tracing;
+using Microsoft.AspNetCore.Mvc;
 
 namespace NextOps.Api.Dtos;
 
 public record class CreateProductDto
 {
    [Required] string Name;
-   string Brand;
+   [Required] string Brand;
    string Model;
    string Description;
 
