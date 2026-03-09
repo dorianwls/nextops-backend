a7e33869f22fbe77c71889f9953460382a58c0e5
Dorian Wilfred Lanuza Smith <dorian.lanuza2022@gmail.com>
2026-03-09 12:51:58 -0600

    Modificacion

--- NextOps.Api/Entities/Product.cs
+++ NextOps.Api/Entities/Product.cs
@@ -6,8 +6,8 @@ public class Product
 {
       public int Id { get; set; }
       public required string Name { get; set; }
-      public string Model { get; set; }
-      public string Brand { get; set; }
+      public string? Model { get; set; }
+      public string? Brand { get; set; }
       public int CategotyId { get; set; }
       public string? Description { get; set; }
       public required bool Status { get; set; }
--- NextOps.Api/Entities/Rol.cs
+++ NextOps.Api/Entities/Rol.cs
@@ -7,6 +7,6 @@ public class Rol
    public required int Id {get; set;}
    public required string Name {get; set;}
    public string? Description {get; set;}
-   public List<User> Users {get; set;}
+   public List<User>? Users {get; set;}
    public required bool Status {get; set;}
 }
