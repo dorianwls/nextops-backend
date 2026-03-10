131468d33b9a9c2ba963b2be6e69a7309f2acb37
Dorian Wilfred Lanuza Smith <dorian.lanuza2022@gmail.com>
2026-03-10 13:41:48 -0600

    feat: arquitectura limpia

---
+++ NextOps.Api/Configurations/EmployeeConfiguration.cs
@@ -0,0 +1,8 @@
+using System;
+
+namespace NextOps.Api.Configurations;
+
+public class EmployeeConfiguration
+{
+
+}
---
+++ NextOps.Api/MenuMapping.cs
@@ -0,0 +1,8 @@
+using System;
+
+namespace NextOps.Api;
+
+public class MenuMapping
+{
+
+}
