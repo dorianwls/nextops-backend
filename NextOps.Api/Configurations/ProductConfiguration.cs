2f4871e4bda928bc6a91c93ca427ae0c372b032a
Dorian Wilfred Lanuza Smith <dorian.lanuza2022@gmail.com>
2026-03-24 12:41:12 -0600

    hla mundo

--- NextOps.Api/Configurations/ProductConfiguration.cs
+++ NextOps.Api/Configurations/ProductConfiguration.cs
@@ -1,8 +1,13 @@
 using System;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using NextOps.Api.Entities;
 
 namespace NextOps.Api.Configurations;
 
 public class ProductConfiguration
 {
-
+   public void Configure(EntityTypeBuilder<Product> builder)
+   {
+      
+   }
 }
