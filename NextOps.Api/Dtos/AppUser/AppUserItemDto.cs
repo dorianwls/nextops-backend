cbb14baeedcd554217c0f6f7dd39a733d9d50dd8
Dorian Wilfred Lanuza Smith <dorian.lanuza2022@gmail.com>
2026-03-31 08:41:01 -0600

    Trabajo en archivos de appuser

--- NextOps.Api/Dtos/AppUser/AppUserItemDto.cs
+++ NextOps.Api/Dtos/AppUser/AppUserItemDto.cs
@@ -1,6 +1,16 @@
 namespace NextOps.Api.Dtos.AppUser;
 
-public record class AppUserItemDtop
+public record class AppUserItemDto
 {
+   public string Id { get; set; } = default!;
+   public string? UserName {get; set;}
+   public string? Email { get; set; }
+   public string? PhoneNumber { get; set; }
+   public string? FirstName { get; set; }
+   public string? MiddleName { get; set; }
+   public string? LastName { get; set; }
+   public string? SecondLastname { get; set; }
+   public string? Title { get; set; }
 
+   public bool Status { get; set; }
 }
--- NextOps.Api/Dtos/AppUser/UpdateAppUserDto.cs
+++ NextOps.Api/Dtos/AppUser/UpdateAppUserDto.cs
@@ -1,6 +1,12 @@
 namespace NextOps.Api.Dtos.AppUser;
 
-public record class UpdateUserDto
+public record class UpdateAppUserDto
 {
+   public string? FirstName { get; set; }
+   public string? MiddleName { get; set; }
+   public string? LastName { get; set; }
+   public string? SecondLastname { get; set; }
+   public string? Title { get; set; }
 
+   public bool Status { get; set; }
 }
